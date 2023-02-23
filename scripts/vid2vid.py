ebb0f14a7eb7a70d057af88544fb326ba916c76d
Filarius <Filarius@users.noreply.github.com>
2023-02-23 21:18:10 +0300

    ugly reworking

--- scripts/vid2vid.py
+++ scripts/vid2vid.py
@@ -12,8 +12,10 @@ from subprocess import Popen, PIPE
 import numpy as np
 from PIL import Image
 from random import randint
+import platform
+import modules
 
-from modules.script_callbacks import on_cfg_denoiser,on_cfg_denoised, CFGDenoiserParams
+from modules.script_callbacks import on_cfg_denoiser,remove_current_script_callbacks
 
 import gradio as gr
 from modules import scripts
@@ -22,7 +24,10 @@ from modules.sd_samplers import sample_to_image
 from modules.sd_samplers_kdiffusion import KDiffusionSampler
 from modules.processing import Processed, process_images, StableDiffusionProcessingImg2Img
 from modules import processing
+from modules.shared import state
 
+
+import json
 
 class LatentMemory:
     def __init__(self, interp_factor=0.1, scale_factor = 0.95):
@@ -64,6 +69,9 @@ class Script(scripts.Script):
         self.img2img_component = gr.Image()
         self.img2img_inpaint_component = gr.Image()
         self.is_have_callback = False
+        #from RIFE_HDv3 import Model
+        #model = Model()
+        #model.load_model('flownet.pkl', -1)
 
 
     # ui components
@@ -84,33 +92,46 @@ class Script(scripts.Script):
             with gr.Row():
                 gr.HTML(value='<div class="text-center block">Latent space temporal blending</div></br>')
             with gr.Row():
-                sfactor  = gr.Slider(
-                    label="Strength",
-                    minimum=0.0,
-                    maximum=1.0,
-                    step=0.01,
-                    value=0.2,
-                )
-                sexp = gr.Slider(
-                    label="Strength scaling (per step)",
-                    minimum=0.9,
-                    maximum=1.1,
-                    step=0.005,
-                    value=1.0,
-                )
+                with gr.Column(min_width=100):
+                        freeze_input_fps = gr.Checkbox(label='Keep input frames', value=False)
+                        keep_fps = gr.Checkbox(label='Keep FPS', value=False)
+                with gr.Column(min_width=100):
+                        sfactor  = gr.Slider(
+                            label="Strength",
+                            minimum=0.0,
+                            maximum=1.0,
+                            step=0.01,
+                            value=0.2,
+                        )
+                        sexp = gr.Slider(
+                            label="Strength scaling (per step)",
+                            minimum=0.9,
+                            maximum=1.1,
+                            step=0.005,
+                            value=1.0,
+                        )
             file.upload(fn=img_dummy_update,inputs=[],outputs=[self.img2img_component])
             tmp_path.change(fn=img_dummy_update,inputs=[],outputs=[self.img2img_component])
-            return [tmp_path, fps, file,sfactor,sexp]
+            return [tmp_path, fps, file,sfactor,sexp,freeze_input_fps,keep_fps]
 
     def after_component(self, component, **kwargs):
         if component.elem_id == "img2img_image":
             self.img2img_component = component
             return self.img2img_component
 
-    def run(self, p:StableDiffusionProcessingImg2Img, file_path, fps, file_obj, sfactor, sexp, *args):
+    def run(self, p:StableDiffusionProcessingImg2Img, file_path, fps, file_obj, sfactor, sexp, freeze_input_fps, keep_fps, *args):
+            save_dir = "outputs/img2img-video/"
+            os.makedirs(save_dir, exist_ok=True)
+            path = modules.paths.script_path
+            if platform.system() == 'Windows':
+                ffmpeg.install(path)
+            import skvideo
+            skvideo.setFFmpegPath(os.path.join(path, "ffmpeg"))
+            import skvideo.io
+
             self.latentmem = LatentMemory(interp_factor=sfactor,scale_factor=sexp)
             if not self.is_have_callback:
-                def callback(params:CFGDenoiserParams):
+                def callback(params):#CFGDenoiserParams
                     self.latentmem.put(params.x)
                     if self.latentmem.flushed:
                         latent = self.latentmem.get()
@@ -149,13 +170,9 @@ class Script(scripts.Script):
                 f"-ss {start_time}" + f" -to {end_time}" if len(end_time) else ""
             )
 
-            import modules
-
-            path = modules.paths.script_path
-            save_dir = "outputs/img2img-video/"
-            ffmpeg.install(path, save_dir)
-
             input_file = os.path.normpath(file_path.strip())
+
+            '''
             decoder = ffmpeg(
                 " ".join(
                     [
@@ -168,10 +185,52 @@ class Script(scripts.Script):
                 ),
                 use_stdout=True,
             )
+            
             decoder.start()
+            '''
+
+            if freeze_input_fps:
+                decoder = skvideo.io.FFmpegReader(input_file)
+            else:
+                decoder = skvideo.io.FFmpegReader(input_file,outputdict={
+                '-r':str(fps)
+            })
+            print()
+            print('FPS ', decoder.inputfps)
+            state.job_count = decoder.inputframenum
+            job_i = 0
+            state.job_no = job_i
 
             output_file = input_file.split("\\")[-1]
             output_file = output_file.split(".")[0]
+            i=1
+            while os.path.isfile(f'{save_dir}/{i}_{output_file}.mp4'):
+                i+=1
+            output_file = f'{save_dir}/{i}_{output_file}.mp4'
+
+            if keep_fps:
+                if '@r_frame_rate' in decoder.probeInfo['video']:
+                    fps = decoder.probeInfo['video']['@r_frame_rate']
+                elif '@avg_frame_rate' in decoder.probeInfo['video']:
+                    fps = decoder.probeInfo['video']['@avg_frame_rate']
+                else:
+                    print('===================================================')
+                    print("[Vid2Vid] Source FPS not known to keep original FPS")
+            if isinstance(fps,str):
+                sp = fps.split('/')
+                if len(sp)==2 and (sp[1]=='1'):
+                    fps = sp[0]
+
+            encoder = skvideo.io.FFmpegWriter(output_file,
+                inputdict={
+                    '-r': str(fps),
+                },
+                outputdict={
+                #'-r':str(fps),
+                '-vcodec': 'libx264',
+                '-crf': '22'
+            })
+            '''
             encoder = ffmpeg(
                 " ".join(
                     [
@@ -185,26 +244,38 @@ class Script(scripts.Script):
                 use_stdin=True,
             )
             encoder.start()
+            '''
 
-            pull_count = p.width * p.height * 3
+            #pull_count = p.width * p.height * 3
             batch = []
+            is_last = False
+            frame_generator = decoder.nextFrame()
+            while not is_last:
 
-            while True:
-                raw_image = decoder.readout(pull_count)
-                if len(raw_image) == 0:
-                    raw_image = None
-                if (raw_image is None) and len(batch)==0:
-                    break
+            #while True:
+                #raw_image = decoder.readout(pull_count)
+                #if len(raw_image) == 0:
+                #    raw_image = None
+                #if (raw_image is None) and len(batch)==0:
+                #    break
 
-                if (raw_image is None):
-                   p.batch_size = len(batch)
+                #if (raw_image is None):
+                #   p.batch_size = len(batch)
+                #else:
+                #if (decoder.inputwidth != p.width) or (decoder.inputheight != p.height):
+                    #image_PIL = Image.fromarray(
+                    #    np.uint8(raw_image).reshape((p.height, p.width, 3)), mode="RGB"
+                    #)
+
+                raw_image = next(frame_generator,[])
+                image_PIL = None
+                if len(raw_image)==0:
+                    is_last = True
                 else:
-                    image_PIL = Image.fromarray(
-                        np.uint8(raw_image).reshape((p.height, p.width, 3)), mode="RGB"
-                    )
+                    image_PIL = Image.fromarray(raw_image,mode='RGB')
                     batch.append(image_PIL)
 
-                if len(batch) == p.batch_size:
+                if (len(batch) == p.batch_size) or ( (len(batch) > 0) and is_last ):
                     p.seed = initial_seed
                     p.init_images = batch
                     batch = []
@@ -216,11 +287,20 @@ class Script(scripts.Script):
                     for output in proc.images:
                         if output.mode != "RGB":
                             output = output.convert("RGB")
-                        encoder.write(np.asarray(output))
+                        encoder.writeFrame(np.asarray(output).copy())
+                        job_i += 1
+                        print(job_i)
+                        print(job_i)
+                        print(job_i)
+                        state.job_no = job_i
 
-            encoder.write_eof()
+            #encoder.write_eof()
+            encoder.close()
+            remove_current_script_callbacks()
+            self.is_have_callback = False
             return Processed(p, [], p.seed, proc.info)
 
+#def get_video_info(path)
 class ffmpeg:
     def __init__(
         self,
@@ -231,7 +311,7 @@ class ffmpeg:
         print_to_console=True,
     ):
         self._process = None
-        self._cmdln = cmdln
+        self._cmdln = cmdln#.split(' ')
         self._stdin = None
 
         if use_stdin:
@@ -253,9 +333,15 @@ class ffmpeg:
         self._process = None
 
     def start(self):
-        self._process = Popen(
-            self._cmdln, stdin=self._stdin, stdout=self._stdout, stderr=self._stderr
-        )
+        try:
+            print(self._cmdln)
+            self._process = Popen(
+                self._cmdln, stdin=self._stdin, stdout=self._stdout, stderr=self._stderr
+            )
+        except Exception as e:
+            print(e)
+
+
 
     def readout(self, cnt=None):
         if cnt is None:
@@ -282,7 +368,7 @@ class ffmpeg:
         return self._process.poll() is None
 
     @staticmethod
-    def install(path, save_dir):
+    def install(path):
         from basicsr.utils.download_util import load_file_from_url
         from zipfile import ZipFile
 
@@ -313,10 +399,12 @@ class ffmpeg:
             os.rmdir(os.path.join(ffmpeg_dir, listOfFileNames[0][:-1], "bin"))
             os.rmdir(os.path.join(ffmpeg_dir, listOfFileNames[0][:-1]))
             print("Downloading FFmpeg: Done")
-        os.makedirs(save_dir, exist_ok=True)
         return
 
     @staticmethod
     def seconds(input="00:00:00"):
         [hours, minutes, seconds] = [int(pair) for pair in input.split(":")]
         return hours * 3600 + minutes * 60 + seconds
+
+
+
